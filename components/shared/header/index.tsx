da4057c8854f33e87e1fdeb3c0bfd11a837a308f
Rahul BK <rlbishwokarma@gmail.com>
2025-01-02 15:12:36 +0545

    Add theme toggle button

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./mode-toggle";
 
 export default function Header() {
   return (
@@ -24,12 +25,13 @@ export default function Header() {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart size={24} /> Cart
             </Link>
           </Button>
-          <Button asChild variant="ghost">
+          <Button asChild>
             <Link href="/sign-in">
               <UserIcon size={24} /> Sign In
             </Link>
